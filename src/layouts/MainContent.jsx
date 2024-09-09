3a948882c2f19c19683cf1ac28cf3967bb47ea47
Emre Şahiner <emre@workintech.com.tr>
2024-09-09 13:20:40 +0300

    fix mainContent width

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -1,5 +1,14 @@
+import Posts from "../components/Posts";
+import TweetForm from "../components/TweetForm";
+
 function MainContent() {
-  return <div className="w-2/4">Main Content</div>;
+  return (
+    <div className="w-2/4 p-4">
+      <h1 className="font-bold text-lg">Home</h1>
+      <TweetForm />
+      <Posts />
+    </div>
+  );
 }
 
 export default MainContent;
