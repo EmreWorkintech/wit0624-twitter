ea9dd19c280c230d95c4c23f9745f52eb0351169
Emre Şahiner <emre@workintech.com.tr>
2024-09-10 12:30:38 +0300

    add vertical lines on both side of content column

--- src/layouts/NavigationBar.jsx
+++ src/layouts/NavigationBar.jsx
@@ -2,7 +2,7 @@ import Navigation from "../components/Navigation";
 
 function NavigationBar() {
   return (
-    <div className="w-1/4 flex flex-col p-4">
+    <div className="w-1/4 flex flex-col p-4 h-screen border-r-2  border-solid border-r-slate-100">
       <i className="fa-brands fa-twitter text-blue-500 text-3xl"></i>
       <Navigation />
       <div className="bg-blue-500 text-center rounded-full text-white p-4 w-3/4">
--- src/layouts/RightSideBar.jsx
+++ src/layouts/RightSideBar.jsx
@@ -1,5 +1,9 @@
 function RightSideBar() {
-  return <div className="w-1/4">RightSideBar</div>;
+  return (
+    <div className="w-1/4 h-screen border-l-2 border-solid border-l-slate-100">
+      RightSideBar
+    </div>
+  );
 }
 
 export default RightSideBar;
