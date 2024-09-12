1b23ac9fd4eb6ea90af800abb26172d33c4acfb1
Emre Şahiner <emre@workintech.com.tr>
2024-09-12 12:12:21 +0300

    add placeholder image for right sidebar

--- src/layouts/RightSideBar.jsx
+++ src/layouts/RightSideBar.jsx
@@ -1,7 +1,9 @@
+import rightSide from "../assets/right.png";
+
 function RightSideBar() {
   return (
     <div className="w-1/4 h-screen border-l-2 border-solid border-l-slate-100">
-      RightSideBar
+      <img className="w-full" src={rightSide} />
     </div>
   );
 }
